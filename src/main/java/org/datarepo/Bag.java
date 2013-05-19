d4beb8ede60b56b1ad51eae386eca2f1360abe77
Rick <richardhightower@gmail.com>
2013-05-19 03:13:41 -0700

    Added comments

--- src/main/java/org/datarepo/Bag.java
+++ src/main/java/org/datarepo/Bag.java
@@ -1,7 +1,9 @@
 package org.datarepo;
 
 /**
+ * Holds stuff
  * @param <ITEM> Type of Stuff you want to hold.
+ */
 public interface Bag <ITEM> {
     void add(ITEM item);
     void remove(ITEM item);
